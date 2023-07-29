2abafee0078c74b7d65d0784bc5f0da46bae95bb
Ekrem <ekrem@gmail.com>
2023-07-29 17:54:34 +0300

    check privet

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import AppRouter from "./router/AppRouter";
 import { lightTheme, darktheme } from "./styles/theme";
 import { GlobalStyles } from "./styles/Global.styles";
 import { useThemeContext } from "./context/ThemeContext";
+import AuthContextProvider from "./context/AuthContext";
 
 function App() {
-  const{myTheme} = useThemeContext();
-  const themes = myTheme === "light" ? lightTheme : darktheme
+  const { myTheme } = useThemeContext();
+  const themes = myTheme === "light" ? lightTheme : darktheme;
 
   return (
     <ThemeProvider theme={themes}>
-      <GlobalStyles/>
-      <AppRouter />
+      <GlobalStyles />
+      <AuthContextProvider>
+        <AppRouter />
+      </AuthContextProvider>
     </ThemeProvider>
   );
 }
