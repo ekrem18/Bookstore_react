321b74f163511305d84f6e86427771d5ac14369b
Ekrem <ekrem@gmail.com>
2023-07-29 13:32:09 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import AppRouter from "./router/AppRouter";
+import { lightTheme } from "./styles/theme";
+import { GlobalStyles } from "./styles/Global.styles";
+
+function App() {
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles/>
+      <AppRouter />
+    </ThemeProvider>
+  );
+}
+
+export default App;
