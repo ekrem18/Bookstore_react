efd4e0f2721f52360ee3a6281b67c0d0b118613f
Ekrem <ekrem@gmail.com>
2023-07-29 14:44:33 +0300

    theme done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import AppRouter from "./router/AppRouter";
-import { lightTheme } from "./styles/theme";
+import { lightTheme, darktheme } from "./styles/theme";
 import { GlobalStyles } from "./styles/Global.styles";
+import { useThemeContext } from "./context/ThemeContext";
 
 function App() {
+  const{myTheme} = useThemeContext();
+  const themes = myTheme === "light" ? lightTheme : darktheme
+
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={themes}>
       <GlobalStyles/>
       <AppRouter />
     </ThemeProvider>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { FooterBtn, FooterContainer } from "./Footer.style";
+import {darkIcon, lightIcon} from "../../helper/iconData"
+import { useThemeContext } from "../../context/ThemeContext";
+
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+const {myTheme,setMyTheme} = useThemeContext();
+
+const handleClick = () => {
+ 
+  setMyTheme(prev => (prev === "light" ? "dark" : "light"));
+    //statein anlık durumuna göre güncelleme yapacak. kullanıcı tıkladıkça toggle mantığı kurmuş olduk
+    // setter metodu içerisine bir callback function kabul edebiliyor. Bu callback function parametre alan bir function ve bu parametre statein anlık değerini temsil ediyor. Bu parametreye göre ekstra işlemler veya kontroller yapabilir
 }
 
-export default Footer
+  return (
+    <FooterContainer>
+      <FooterBtn onClick={handleClick}>{myTheme === "light" ? lightIcon : darkIcon}</FooterBtn>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
