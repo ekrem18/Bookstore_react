c19ce930b04144c84e6d81683715583d2c65f86f
Ekrem <ekrem@gmail.com>
2023-07-31 12:10:51 +0300

    router and style

--- src/components/navbar/Navbar.style.jsx
+++ src/components/navbar/Navbar.style.jsx
@@ -1,28 +1,30 @@
 // Navbar Style
 import styled from "styled-components"
+import Flex from "../../styles/Flex";
+import { NavLink } from "react-router-dom";
 
-export const Nav =styled.div`
+export const Nav =styled(Flex)`
 
-`
+`;
 
 
-export const Logo = styled.div`
+export const Logo = styled(NavLink)`
 
-`
+`;
 
 
 export const HamburgerIcon = styled.div`
 
-`
+`;
 
 
 export const Menu = styled.div`
 
-`
+`;
 
 
 
-export const MenuLink = styled.div`
+export const MenuLink = styled(NavLink)`
 
-`
+`;
 
---
+++ src/styles/Flex.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Flex = styled.div`
+  display: flex;
+  justify-content:${(justify)=> justify || "center" };
+  align-items: ${(align)=>align || "center"};
+  flex-wrap: ${(wrap)=> wrap || "nowrap"}
+`;
+
+export default Flex;
+
