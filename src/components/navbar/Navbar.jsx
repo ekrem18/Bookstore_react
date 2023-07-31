2aa29fd6c8e2769f6d0ed16fd176ea245139cf7a
Ekrem <ekrem@gmail.com>
2023-07-31 12:04:04 +0300

    navbar edits

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { menuIcon } from '../../helper/iconData'
+import { HamburgerIcon, Logo, Menu, MenuLink, Nav } from './Navbar.style'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Nav >
+      <Logo>Clarusway Library</Logo>
+      <HamburgerIcon>{menuIcon}</HamburgerIcon>
+      <Menu>
+        <MenuLink>Home</MenuLink>
+        <MenuLink>Home</MenuLink>
+        <MenuLink>Home</MenuLink>
+        <MenuLink>Home</MenuLink>
+        <MenuLink>Home</MenuLink>
+      </Menu>
+      </Nav>
   )
 }
 
--- src/components/navbar/Navbar.style.jsx
+++ src/components/navbar/Navbar.style.jsx
@@ -1,1 +1,28 @@
 // Navbar Style
+import styled from "styled-components"
+
+export const Nav =styled.div`
+
+`
+
+
+export const Logo = styled.div`
+
+`
+
+
+export const HamburgerIcon = styled.div`
+
+`
+
+
+export const Menu = styled.div`
+
+`
+
+
+
+export const MenuLink = styled.div`
+
+`
+
