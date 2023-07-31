bbd3b94f2addff930a1b4f2886c057a2ce93d40d
Ekrem <avekremyilmazturk@gmail.com>
2023-07-31 17:47:30 +0300

    container struct.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Header } from '../login/Login.style'
+import { CardContainer, HomeContainer, HomeImage } from './Home.style'
+import homeImg from "../../assets/books.jpg"
 
 const Home = () => {
   return (
     <HomeContainer>
       <Header/>
+      <HomeImage>
+        <img src={homeImg} />
+      </HomeImage>
+      <CardContainer>
+        <Card/>
+      </CardContainer>
     </HomeContainer>
   )
 }
