ce55cccfb0f95fb81583617b7ff00e81547ffa1c
Ekrem <ekrem@gmail.com>
2023-07-31 11:36:38 +0300

    editsome

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,8 +10,7 @@ const {myTheme,setMyTheme} = useThemeContext();
 const handleClick = () => {
  
   setMyTheme(prev => (prev === "light" ? "dark" : "light"));
-    //statein anlık durumuna göre güncelleme yapacak. kullanıcı tıkladıkça toggle mantığı kurmuş olduk
-    // setter metodu içerisine bir callback function kabul edebiliyor. Bu callback function parametre alan bir function ve bu parametre statein anlık değerini temsil ediyor. Bu parametreye göre ekstra işlemler veya kontroller yapabilir
+    
 }
 
   return (
