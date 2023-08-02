0adffd17cabb8472bd564abe876d756143138e71
Ekrem <avekremyilmazturk@gmail.com>
2023-08-02 16:01:57 +0300

    sth happening

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { HeaderButton, HeaderContainer, HeaderForm, HeaderTitle, SearchInput, SelectBox } from './Header.style'
+import Flex from '../../styles/Flex'
 
 const Header = () => {
   return (
     <HeaderContainer>
       <HeaderTitle>Books or Magazines</HeaderTitle>
       <HeaderForm>
+        <Flex>
         <SearchInput type="search" placeholder="Search..." />
         <SelectBox>
           <option>All</option>
         </SelectBox>
+        </Flex>
         <HeaderButton>Search</HeaderButton>
       </HeaderForm>
     </HeaderContainer>
--- src/components/header/Header.style.jsx
+++ src/components/header/Header.style.jsx
@@ -5,7 +5,6 @@ import Flex from "../../styles/Flex";
 export const HeaderContainer = styled(Flex)`
   flex-direction: column;
 `;
-
 export const HeaderTitle = styled.h1`
   margin-top: 1rem;
   font-size: 3rem;
@@ -14,10 +13,11 @@ export const HeaderTitle = styled.h1`
     font-size: 1.6rem;
   }
 `;
-
 export const HeaderForm = styled.form`
   display: flex;
-  width: 45%;
+  width: 100%;
+  flex-direction: column;
+  padding: 1rem;
   flex-wrap: wrap;
   justify-content: center;
   border-radius: 16px;
@@ -27,14 +27,62 @@ export const HeaderForm = styled.form`
   margin: 20px;
   background-color: ${({ theme }) => theme.colors.headerFormColor};
   @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    /* background-color: black; */
     flex-direction: column;
     width: 90%;
     padding: 1rem;
   }
 `;
 
-export const SearchInput = styled.input``;
+export const SearchInput = styled.input`
+  height: 3rem;
+  width: 55%;
+  border: none;
+  margin-top: 2rem;
+  border-radius: 3px;
+  outline: none;
+  text-indent: 10px;
+  font-size: 2rem;
+  border-top-left-radius: 1rem;
+  border-bottom-left-radius: 1rem;
+  background-color: rgb(255, 255, 255, 0.5);
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    width: 100%;
+    border-radius: 5px;
+  }
+`;
 
-export const SelectBox = styled.select``;
+export const SelectBox = styled.select`
+  border-radius: 3px;
+  margin-top: 2rem;
+  padding: 5px;
+  height: 3rem;
+  width: 70%;
+  border: none;
+  outline: none;
+  font-size: 2rem;
+  border-top-right-radius: 1rem;
+  background-color: rgb(255, 255, 255, 0.8);
+  border-bottom-right-radius: 1rem;
+  @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    width: 100%;
+    border-radius: 5px;
+  }
+`;
 
-export const HeaderButton = styled.button``;
+export const HeaderButton = styled.button`
+  background-color: #e1f1dd;
+  padding: 5px;
+  outline: none;
+  height: 3rem;
+  border: none;
+  margin: 1rem;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 2rem;
+  border-top-left-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.navbarBgColor};
+  }
+`;
